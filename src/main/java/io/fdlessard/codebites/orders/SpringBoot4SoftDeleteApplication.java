1142256a08220c830c1e93ea55e260e38fe7391d
fdlessard <fdlessard@gmail.com>
2026-02-11 19:47:42 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/orders/SpringBoot4SoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4SoftDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4SoftDeleteApplication.class, args);
+    }
+
+}
